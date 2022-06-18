f1291bed01f4d3f2f7d247af95eb0685cc19f13c
zqtay <zqtay96@gmail.com>
2022-06-18 17:22:41 +0800

    File export

--- app/src/main/java/com/example/myapp/Util.java
+++ app/src/main/java/com/example/myapp/Util.java
@@ -2,11 +2,26 @@ package com.example.myapp;
 
 import com.example.myapp.db.BGRecord;
 
+import android.content.Context;
+import android.net.Uri;
 import android.util.Log;
 
 import com.example.myapp.db.BGRecord;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.nio.file.FileAlreadyExistsException;
+import java.nio.file.FileSystemAlreadyExistsException;
+import java.util.List;
+
 public class Util {
+
+    public final static int RESULT_SUCCESS = 0;
+    public final static int RESULT_FAILED = -1;
+
 
     public static byte convertEvent(String event1) {
         byte event = (byte)0;
@@ -74,5 +89,62 @@ public class Util {
         Log.d("debug","BG_Post: " + record.bglevel_post);
         Log.d("debug","Dose: " + record.dose);
         Log.d("debug","Notes: " + record.notes);
+    }
+
+    public static String getRecordsText(List<BGRecord> records) {
+        String recordsText = "";
+        String recordLine = "";
+
+        if (records == null) {
+            return recordsText;
+        }
+
+        for (int i = 0; i < records.size(); i++) {
+            recordLine = "";
+            recordLine += String.valueOf(records.get(i).date);
+            recordLine += (",");
+            recordLine += String.valueOf(records.get(i).event);
+            recordLine += (",");
+            recordLine += (records.get(i).bglevel_pre != null) ? String.format("%.01f", records.get(i).bglevel_pre) : null;
+            recordLine += (",");
+            recordLine += (records.get(i).bglevel_post != null) ? String.format("%.01f", records.get(i).bglevel_post) : null;
+            recordLine += (",");
+            recordLine += (records.get(i).dose != null) ? String.format("%.01f", records.get(i).dose) : null;
+            recordLine += (",");
+            recordLine += records.get(i).notes;
+            recordLine += ("\n");
+            recordsText += recordLine;
+        }
+
+        return recordsText;
+    }
+
+    public static int writeToFile(String data, String filePath, String fileName) {
+        int result = RESULT_FAILED;
+        File fileDir = new File(filePath);
+        File file = new File(filePath, fileName);
+        if (!fileDir.exists()) {
+            fileDir.mkdir();
+        }
+        try {
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+        }
+        catch (IOException e) {
+            Log.e("Exception", "File creation failed: " + e.toString());
+            result = RESULT_FAILED;
+        }
+        try {
+            FileOutputStream stream = new FileOutputStream(file);
+            stream.write(data.getBytes());
+            stream.close();
+            result = RESULT_SUCCESS;
+        }
+        catch (IOException e) {
+            Log.e("Exception", "File write failed: " + e.toString());
+            result = RESULT_FAILED;
+        }
+        return result;
     }
 }
--- app/src/main/java/com/example/myapp/fragment/ViewFragment.java
+++ app/src/main/java/com/example/myapp/fragment/ViewFragment.java
@@ -2,27 +2,39 @@ package com.example.myapp.fragment;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.DocumentsContract;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.example.myapp.FileUtil;
 import com.example.myapp.R;
+import com.example.myapp.Util;
 import com.example.myapp.db.AppDatabaseService;
 import com.example.myapp.db.BGRecord;
 
+import java.io.File;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.List;
 
 public class ViewFragment extends Fragment {
+    public static final int ACTIVITY_REQUEST_EXPORT_FILE = 0x9999;
+    public static final String BGRECORD_HEADER = "date,event,bglevel_pre,bglevel_post,dose,notes\n";
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -35,15 +47,18 @@ public class ViewFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        view.findViewById(R.id.button_findAll).setOnClickListener(this::onClickFindAll);
+        view.findViewById(R.id.button_export).setOnClickListener(this::onClickExport);
+
         AsyncTask.execute(()-> {
             List<BGRecord> records = AppDatabaseService.findAllRecord(getActivity().getApplicationContext());
             getActivity().runOnUiThread( ()-> {
-                String textDisplay = getRecordsText(records);
+                String textDisplay = Util.getRecordsText(records);
                 if (textDisplay.isEmpty()) {
                     textDisplay = "No records found";
                 }
                 else {
-                    textDisplay = "date,event,bglevel_pre,bglevel_post,dose,notes\n" + textDisplay;
+                    textDisplay = BGRECORD_HEADER + textDisplay;
                 }
 
                 ((TextView) view.findViewById(R.id.textView_view)).setText(textDisplay);
@@ -54,49 +69,75 @@ public class ViewFragment extends Fragment {
 
     }
 
-    private String getRecordsText(List<BGRecord> records) {
-        String recordsText = "";
-        String recordLine = "";
-
-        if (records == null) {
-            return recordsText;
-        }
-
-        for (int i = 0; i < records.size(); i++) {
-            recordLine = "";
-            recordLine += String.valueOf(records.get(i).date);
-            recordLine += (",");
-            recordLine += String.valueOf(records.get(i).event);
-            recordLine += (",");
-            recordLine += (records.get(i).bglevel_pre != null) ? String.format("%.01f", records.get(i).bglevel_pre) : null;
-            recordLine += (",");
-            recordLine += (records.get(i).bglevel_post != null) ? String.format("%.01f", records.get(i).bglevel_post) : null;
-            recordLine += (",");
-            recordLine += (records.get(i).dose != null) ? String.format("%.01f", records.get(i).dose) : null;
-            recordLine += (",");
-            recordLine += records.get(i).notes;
-            recordLine += ("\n");
-            recordsText += recordLine;
-        }
-
-        return recordsText;
-    }
-
     public void onClickFindAll(View v) {
         Activity activity = getActivity();
         AsyncTask.execute(()-> {
             List<BGRecord> records = AppDatabaseService.findAllRecord(activity.getApplicationContext());
             getActivity().runOnUiThread( ()-> {
-                String textDisplay = getRecordsText(records);
+                String textDisplay = Util.getRecordsText(records);
                 if (textDisplay.isEmpty()) {
                     textDisplay = "No records found";
                 }
                 else {
-                    textDisplay = "date,event,bglevel_pre,bglevel_post,dose,notes\n" + textDisplay;
+                    textDisplay = BGRECORD_HEADER + textDisplay;
                 }
 
                 ((TextView) activity.findViewById(R.id.textView_view)).setText(textDisplay);
             });
         });
+    }
+
+    public void onClickExport(View v) {
+        Activity activity = getActivity();
+        Context context = activity.getApplicationContext();
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP){
+            Intent i = new Intent(Intent.ACTION_OPEN_DOCUMENT_TREE);
+            i.addCategory(Intent.CATEGORY_DEFAULT);
+            startActivityForResult(Intent.createChooser(i, "Choose directory"), ACTIVITY_REQUEST_EXPORT_FILE);
+        }
+
+
+    }
+
+    public void exportFile(Uri exportUri) {
+        Activity activity = getActivity();
+        Context context = getActivity().getApplicationContext();
+        String exportPath = FileUtil.getFullPathFromTreeUri(exportUri, context);
+        Log.d("debug",exportPath);
+        AsyncTask.execute(()-> {
+            List<BGRecord> records = AppDatabaseService.findAllRecord(activity.getApplicationContext());
+            String textDisplay = BGRECORD_HEADER + Util.getRecordsText(records);
+            int exportResult = Util.writeToFile(textDisplay, exportPath, "BGRecord.txt");
+            getActivity().runOnUiThread(() -> {
+                if (exportResult == Util.RESULT_SUCCESS) {
+                    Toast.makeText(context,
+                                    "File exported to: " + exportPath + "\\" + "BGRecord.txt",
+                                    Toast.LENGTH_SHORT)
+                            .show();
+                }
+                else {
+                    Toast.makeText(context,
+                                    "File export failed",
+                                    Toast.LENGTH_SHORT)
+                            .show();
+                }
+            });
+        });
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch(requestCode) {
+            case ACTIVITY_REQUEST_EXPORT_FILE:
+                Uri exportUri = data.getData();
+                Log.i("Test", "Result URI " + exportUri);
+                if (exportUri != null) {
+                    exportFile(exportUri);
+                }
+                break;
+        }
     }
 }
